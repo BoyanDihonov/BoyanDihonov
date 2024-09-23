0f6a9bba620efcbcb3a70bb0c98de419c7f372b5
Boyan Dihonov <boyandihonov@abv.bg>
2024-09-23 21:12:17 +0300

    Update scripct.js
    Added function updateClock

--- script.js
+++ script.js
@@ -9,3 +9,16 @@ function showContent(tab) {
     navLinks.forEach(link => link.classList.remove('active'));
     document.querySelector(`nav a[onclick="showContent('${tab}')"]`).classList.add('active');
 }
+function updateClock() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    const timeString = `${hours}:${minutes}:${seconds}`;
+
+    document.getElementById('clock').innerText = timeString;
+}
+
+setInterval(updateClock, 1000);
+
+updateClock();
