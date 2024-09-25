4dda67e03949e4412ea0e9849b515f8c3a8fe01f
Boyan Dihonov <boyandihonov@abv.bg>
2024-09-25 11:03:05 +0300

    Update script.js
    added date

--- script.js
+++ script.js
@@ -4,21 +4,26 @@ function showContent(tab) {
 
     document.getElementById(tab).classList.add('active');
 
-    // Update navigation links
     const navLinks = document.querySelectorAll('nav a');
     navLinks.forEach(link => link.classList.remove('active'));
     document.querySelector(`nav a[onclick="showContent('${tab}')"]`).classList.add('active');
 }
+
 function updateClock() {
     const now = new Date();
     const hours = String(now.getHours()).padStart(2, '0');
     const minutes = String(now.getMinutes()).padStart(2, '0');
     const seconds = String(now.getSeconds()).padStart(2, '0');
-    const timeString = `${hours}:${minutes}:${seconds}`;
+    const dayOfWeek = now.toLocaleString('en-US', { weekday: 'long' });
+    const day = String(now.getDate()).padStart(2, '0');
+    const month = now.toLocaleString('en-US', { month: 'long' });
+    const year = now.getFullYear();
 
-    document.getElementById('clock').innerText = timeString;
+    const timeString = `${hours}:${minutes}:${seconds}`;
+    const dateString = `${dayOfWeek}, ${month} ${day}, ${year}`;
+
+    document.getElementById('clock').innerHTML = `${timeString}<br>${dateString}`;
 }
 
 setInterval(updateClock, 1000);
-
 updateClock();
