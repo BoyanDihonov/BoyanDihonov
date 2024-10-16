6ff0441ab9e89a36d0e83a9c40db974f1f220da4
Boyan Dihonov <boyandihonov@abv.bg>
2024-10-16 13:33:57 +0300

    Updated the appearance

--- pages/IzoStroi/scripts.js
+++ pages/IzoStroi/scripts.js
@@ -1,15 +1,3 @@
-document.querySelectorAll('nav a').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        const href = this.getAttribute('href');
-        if (href.startsWith('#')) {  
-            e.preventDefault();
-            const targetId = href.substring(1);
-            const targetSection = document.getElementById(targetId);
-            if (targetSection) {
-                targetSection.scrollIntoView({
-                    behavior: 'smooth'
-                });
-            }
-        }
-    });
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("Уеб страницата е заредена и готова!");
 });
