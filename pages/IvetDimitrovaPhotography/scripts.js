95a1eef00d35fcbae2070b037142fed855b3b6d8
Boyan Dihonov <boyandihonov@abv.bg>
2024-10-31 22:12:24 +0200

    Update scripts.js

--- pages/IvetDimitrovaPhotography/scripts.js
+++ pages/IvetDimitrovaPhotography/scripts.js
@@ -7,4 +7,34 @@ document.addEventListener('DOMContentLoaded', function() {
             link.classList.add('active');
         }
     });
+
+    const modal = document.getElementById("myModal");
+    const modalImage = document.getElementById("modalImage");
+    const captionText = document.getElementById("caption");
+    const images = document.querySelectorAll('.gallery-item');
+    let currentIndex;
+
+    images.forEach((image, index) => {
+        image.onclick = function() {
+            modal.style.display = "block";
+            modalImage.src = this.src;
+            captionText.innerHTML = this.alt;
+            currentIndex = index;
+        };
+    });
+
+    document.getElementsByClassName("close")[0].onclick = function() {
+        modal.style.display = "none";
+    };
+
+    window.changeSlide = function(n) {
+        currentIndex += n;
+        if (currentIndex < 0) {
+            currentIndex = images.length - 1;
+        } else if (currentIndex >= images.length) {
+            currentIndex = 0;
+        }
+        modalImage.src = images[currentIndex].src;
+        captionText.innerHTML = images[currentIndex].alt;
+    };
 });
