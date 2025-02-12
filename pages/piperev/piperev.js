0f82a123a14568c9e7b2b4ff5b4e6898e9fd4eb3
Boyan Dihonov <boyandihonov@abv.bg>
2025-02-12 11:46:36 +0200

    Deleting a page

--- pages/piperev/piperev.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('ctaButton').addEventListener('click', function() {
-    alert('Записването за услуга ще бъде на нашия офис! Очаквайте скоро повече информация.');
-});
-
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Вашето съобщение беше изпратено успешно!');
-});
